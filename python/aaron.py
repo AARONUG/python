514f0b37824bf2587ac75dbe13ab7d44b94a7298
AARONUG <aaronkwesiga20@gmail.com>
2024-10-15 11:27:12 +0300

    Add files via upload

---
+++ python/aaron.py
@@ -0,0 +1,18 @@
+class Dog:
+    spacies = "canis lupus familaris"
+
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+        def bark(self):
+            return "woof!"
+        
+my_dog = Dog("buddy",3)
+print(my_dog.name)
+print(my_dog.age)
+print(my_dog.spacies)
+
+print(my_dog.bark())
+
+print("aaron")
